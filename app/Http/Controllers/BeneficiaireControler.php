41073dd27ef34298679434bd4ef27232e294981c
aminaelbayyadi <elbayyadiamina@gmail.com>
2023-05-04 14:59:34 +0100

    table benefiaire

---
+++ app/Http/Controllers/BeneficiaireControler.php
@@ -0,0 +1,14 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use App\Models\Beneficiaire;
+use DataTables;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+ 
+class BeneficiaireControler extends Controller
+{
+    
+}
