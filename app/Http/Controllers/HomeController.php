4ecddbe1fb566a22bc5570f807c501a73307b70e
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2023-05-14 15:42:35 +0000

    Add charts to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 use App\Models\Fournisseur;
 use App\Models\Produit;
 use App\Models\Stock;
+use App\Models\reception;
+use Illuminate\Support\Facades\Session;
+use App\Charts\UserChart;
 
 use Illuminate\Http\Request;
 
@@ -26,12 +29,48 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $totalProducts = Produit::count();
-        $totalFournisseurs = Stock::count();
+        $totalProduits = Produit::count();
+        $totalFournisseurs = Fournisseur::count();
         
+        $receptionsData = Reception::selectRaw("DATE_FORMAT(datereception, '%m/%Y') as month_year, count(*) as count")
+        ->groupBy('month_year')
+        ->get();
+    
+        $labels = $receptionsData->pluck('month_year');
+        $counts = $receptionsData->pluck('count');
+    
+        $receptionsChart = session('receptionsChart');
+        $receptionsChart = new UserChart;
+        $receptionsChart->labels($labels);
+        $receptionsChart->dataset('Nombre de receptions', 'bar', $counts);
+        $receptionsChart->options([
+            'title' => [
+                'display' => true,
+                'text' => 'Nombre de receptions par mois'
+            ],
+            'scales' => [
+                'xAxes' => [
+                    [
+                        'scaleLabel' => [
+                            'display' => true,
+                            'labelString' => 'Mois'
+                        ]
+                    ]
+                ],
+                'yAxes' => [
+                    [
+                        'scaleLabel' => [
+                            'display' => true,
+                            'labelString' => 'Nombre de receptions'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+        session(['receptionsChart' => $receptionsChart]);
+
         
-        
-        return view('home', compact('totalProducts', 'totalFournisseurs'));
+        return view('home', [ 'receptionsChart' => $receptionsChart ] );
     }
 }
 
